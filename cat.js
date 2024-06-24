4373871795a4a15dbce9076934955316cdd0fbbb
poojasrisekar <poojoo.shree@gmail.com>
2024-06-24 17:38:45 +0530

    Create cat.js

---
+++ cat.js
@@ -0,0 +1,13 @@
+fetch('https://alexwohlbruck.github.io/cat-facts/')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Data received:', data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
